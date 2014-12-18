56ea3cf06168238f5e897d3fb7dba019326b3e0c
František Dvořák <valtri@civ.zcu.cz>
2014-12-19 00:27:27 +0100

    Separate class for Cloudera, II.

--- manifests/config.pp
+++ manifests/config.pp
@@ -9,25 +9,4 @@ class site_hadoop::config {
       target => "${site_hadoop::mc_setup}.sh",
     }
   }
-
-  if $::osfamily == 'Debian' {
-    # cloudera repo
-    exec { 'key-cloudera':
-      command => 'apt-key adv --fetch-key http://archive.cloudera.com/cdh5/debian/wheezy/amd64/cdh/archive.key',
-      path    => $site_hadoop::path,
-      creates => '/etc/apt/sources.list.d/cloudera.list',
-    }
-    ->
-    exec { 'wget-cloudera':
-      command => 'wget -P /etc/apt/sources.list.d/ http://archive.cloudera.com/cdh5/debian/wheezy/amd64/cdh/cloudera.list',
-      path    => $site_hadoop::path,
-      creates => '/etc/apt/sources.list.d/cloudera.list',
-    }
-    ~>
-    exec { 'apt-get-update':
-      command     => 'apt-get update',
-      refreshonly => true,
-      path        => $site_hadoop::path,
-    }
-  }
 }
