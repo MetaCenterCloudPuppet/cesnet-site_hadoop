f5d593b9ab0ecce9bfd21e4274b7f14648b39a50
František Dvořák <valtri@civ.zcu.cz>
2014-12-18 18:10:18 +0100

    Mail not needed (in Meta puppet).

--- manifests/init.pp
+++ manifests/init.pp
@@ -5,12 +5,10 @@
 class site_hadoop inherits site_hadoop::params {
   include 'site_hadoop::install'
   include 'site_hadoop::config'
-  include 'site_hadoop::mail'
 
   Class['site_hadoop::install'] ->
   Class['site_hadoop::config'] ->
   Class['site_hadoop']
 
-  Class['site_hadoop::mail'] ->
   Class['site_hadoop']
 }
