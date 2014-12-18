b2cabd6ac952068881a62ebac6b0c009eede2680
František Dvořák <valtri@civ.zcu.cz>
2014-12-18 19:32:00 +0100

    Class for hwraid repository and megacli, separate class for cloudera.

--- manifests/init.pp
+++ manifests/init.pp
@@ -5,7 +5,9 @@
 class site_hadoop inherits site_hadoop::params {
   include 'site_hadoop::install'
   include 'site_hadoop::config'
+  include 'site_hadoop::cloudera'
 
+  Class['site_hadoop::cloudera'] ->
   Class['site_hadoop::install'] ->
   Class['site_hadoop::config'] ->
   Class['site_hadoop']
