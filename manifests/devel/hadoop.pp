381bd25fba3b3d1f5d9b08af26faf34cf301ee3c
František Dvořák <valtri@civ.zcu.cz>
2014-12-29 01:30:32 +0100

    Helper class for development under Vagrant.

---
+++ manifests/devel/hadoop.pp
@@ -0,0 +1,19 @@
+# == Class site_hadoop::devel::hadoop
+# Local post-installation steps for Hadoop.
+class site_hadoop::devel::hadoop {
+  hadoop::kinit{'vagrant-kinit':
+    touchfile => 'vagrant-user-created',
+  }
+  ->
+  hadoop::mkdir{'/user/vagrant':
+    owner     => 'vagrant',
+    group     => 'hadoop',
+    mode      => '0750',
+    touchfile => 'vagrant-user-created',
+  }
+  ->
+  hadoop::kdestroy{'vagrant-kdestroy':
+    touchfile => 'vagrant-user-created',
+    touch     => true,
+  }
+}
