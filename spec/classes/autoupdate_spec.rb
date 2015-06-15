48b6754e830cc12e93672361edf6a0b25689282d
František Dvořák <valtri@civ.zcu.cz>
2015-06-15 08:44:02 +0200

    Travis CI integration - unit tests, update from the latest puppet skeleton.

---
+++ spec/classes/autoupdate_spec.rb
@@ -0,0 +1,15 @@
+require 'spec_helper'
+
+describe 'site_hadoop::autoupdate', :type => 'class' do
+  context "on Debian" do
+    let(:facts) { {:operatingsystem => 'Debian', :osfamily => 'Debian' } }
+
+    it { should compile.with_all_deps }
+    it { should contain_file('/etc/cron.d/cron-apt') }
+  end
+  context "on non-Debian" do
+    let(:facts) { {:operatingsystem => 'Fedora', :osfamily => 'RedHat' } }
+
+    it { should compile.with_all_deps }
+  end
+end
---
+++ spec/classes/site_hadoop_spec.rb
@@ -0,0 +1,15 @@
+require 'spec_helper'
+
+describe 'site_hadoop', :type => 'class' do
+  $test_os.each do |facts|
+    os = facts['operatingsystem']
+
+    context "on #{os}" do
+      let(:facts) do
+        facts
+      end
+      it { should compile.with_all_deps }
+      it { should contain_class('site_hadoop::cloudera') }
+    end
+  end
+end
